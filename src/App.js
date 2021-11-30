0148ffae8a8fd83e9d6a6b74a7dc0a7197fc71f2
Danhalf <danhel@rocketmail.com>
2021-11-30 15:47:12 +0200

    redirect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import style from './App.module.css';
 import Employees from './Employees/Employees';
 import TrackingEmployees from './TrackingEmployees/TrackingEmployees';
-import { setEmployees, addActive, deleteActive } from "./store/employeesReducer";
-import { connect } from "react-redux";
+import {
+  setEmployees,
+  addActive,
+  deleteActive,
+} from './store/employeesReducer';
+import { connect } from 'react-redux';
 
 function App() {
-    return (
-        <div className={ style.app }>
-            <EmployeesContainer/>
-            <TrackingEmployeesContainer/>
-        </div>
-    );
+  return (
+    <div className={style.app}>
+      <EmployeesContainer />
+      <TrackingEmployeesContainer />
+    </div>
+  );
 }
 
-const mapStateToProps = state => ({
-    employees: state.employees,
-    trackingEmployees: state.trackingEmployees
+const mapStateToProps = (state) => ({
+  employees: state.employees,
+  trackingEmployees: state.trackingEmployees,
 });
 
-
-const EmployeesContainer = connect(mapStateToProps, {setEmployees, addActive, deleteActive})(Employees)
-const TrackingEmployeesContainer = connect(mapStateToProps, {toggleActive: addActive})(TrackingEmployees)
+const EmployeesContainer = connect(mapStateToProps, {
+  setEmployees,
+  addActive,
+  deleteActive,
+})(Employees);
+const TrackingEmployeesContainer = connect(mapStateToProps, {
+  toggleActive: addActive,
+})(TrackingEmployees);
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import store from "./store/store";
+import store from './store/store';
 
+if (!window.location.href.includes('employees/'))
+  window.location.href += 'employees/';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={ store }>
-            <App/>
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/employees" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
