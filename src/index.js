5251558d61c7e4f9ce3eead77ae2deef38e0741a
Danhalf <danhel@rocketmail.com>
2021-11-30 16:22:17 +0200

    ref

--- src/index.js
+++ src/index.js
@@ -7,15 +7,12 @@ import './index.css';
 import App from './App';
 import store from './store/store';
 
-if (!window.location.href.includes('employees/'))
-  window.location.href += 'employees/';
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/employees" element={<App />} />
+          <Route path="/" element={<App />} />
         </Routes>
       </BrowserRouter>
     </Provider>
